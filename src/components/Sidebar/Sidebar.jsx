d9a4f384cfbb8ee4d1992f77b30470277dbe5b88
OrazovTairlan <orazovtairlan@gmail.com>
2022-09-19 16:43:03 +0600

    Added sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { FaDiscord } from 'react-icons/fa';
+
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-white dark:bg-gray-900 shadow-lg">
+            <SideBarIcon icon={<FaDiscord size="28" />} />
+            <SideBarIcon icon={<FaDiscord size="28" />} />
+            <SideBarIcon icon={<FaDiscord size="28" />} />
+            <SideBarIcon icon={<FaDiscord size="28" />} />
+            <SideBarIcon icon={<FaDiscord size="28" />} />
+        </div>
+    );
+};
+
+export const SideBarIcon = ({ icon, text = 'tooltip' }) => (
+    <div className="sidebar-icon group">
+        {icon}
+        <span class="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+    </div>
+);
+
+
+export const Divider = () => <hr className="sidebar-hr" />;
+
+export default SideBar;
