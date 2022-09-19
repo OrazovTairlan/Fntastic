07e0e54917c5b3715310ab9fd4c553b7d8381285
OrazovTairlan <orazovtairlan@gmail.com>
2022-09-19 19:13:53 +0600

    composition of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SideBar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
+import ChannelBar from "./components/Content/Content";
+import Messages from "./components/Messages/Messages";
+import './index.css';
 function App() {
   return (
-      <div className="flex">
-
+      <div className="flex h-screen">
+          <SideBar/>
+          <ChannelBar/>
+          <Messages/>
       </div>
   );
 }
