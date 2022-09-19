19f57ed0b4d314e08e93accaaf44e5c440e92bd1
OrazovTairlan <orazovtairlan@gmail.com>
2022-09-19 19:16:57 +0600

    added sidebar for right side

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,12 +13,31 @@ const SideBar = () => {
     );
 };
 
+export const SideBarRight = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-white dark:bg-gray-900 shadow-lg">
+            <SideBarUser icon={<FaDiscord size="28" />} />
+            <SideBarUser icon={<FaDiscord size="28" />} />
+            <SideBarUser icon={<FaDiscord size="28" />} />
+            <SideBarUser icon={<FaDiscord size="28" />} />
+            <SideBarUser icon={<FaDiscord size="28" />} />
+        </div>
+    );
+};
+
 export const SideBarIcon = ({ icon, text = 'tooltip' }) => (
     <div className="sidebar-icon group">
         {icon}
         <span class="sidebar-tooltip group-hover:scale-100">
       {text}
     </span>
+    </div>
+);
+
+export const SideBarUser = ({ icon }) => (
+    <div className="sidebar-icon group">
+        {icon}
     </div>
 );
 
